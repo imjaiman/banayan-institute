be10601355c2545eb149111d40a74f74b9ddaa5f
13219026 <rahul.jaiman@africa.airtel.com>
2022-02-21 00:21:44 +0530

    entity

---
+++ src/main/java/com/codescad/education/institute/model/entity/DocumentEntity.java
@@ -0,0 +1,19 @@
+package com.codescad.education.institute.model.entity;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+/**
+ * @author Rahul Jaiman
+ * @date 2022-02-21 12:14 AM
+ */
+
+
+@Data
+@Entity
+@Table(name = "DOCUMENT")
+public class DocumentEntity {
+
+}
---
+++ src/main/java/com/codescad/education/institute/model/entity/ReportEntity.java
@@ -0,0 +1,44 @@
+package com.codescad.education.institute.model.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+/**
+ * @author Rahul Jaiman
+ * @date 2022-02-21 12:16 AM
+ */
+
+
+@Data
+@Entity
+@Table(name = "REPORT")
+public class ReportEntity {
+
+    @Id
+    @Column(name = "ID")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "student_id_seq")
+    @SequenceGenerator(allocationSize = 1, name = "student_id_seq", sequenceName = "student_id_seq")
+    private Long id;
+
+    @Column(name = "BATCH_ID")
+    private String batchId;
+
+    @Column(name = "EXAM_TYPE")
+    private String examType;
+
+    @Column(name = "GRADE")
+    private String grade;
+
+    @Column(name = "MAX_MARKS")
+    private String maxMarks;
+
+    @Column(name = "MIN_MARKS")
+    private String minMarks;
+
+    @Column(name = "MARKS")
+    private String marks;
+
+    @Column(name = "SUBJECT")
+    private String subject;
+}
